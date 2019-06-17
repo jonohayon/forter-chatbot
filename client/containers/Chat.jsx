073257b297c92edfcd25f842329bd812a14565b7
Jonathan Ohayon <me@jonathano.com>
2019-06-17 14:39:22 +0300

    Added some test UI

--- client/containers/Chat.jsx
+++ client/containers/Chat.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import NavBar from 'components/NavBar.jsx'
+import Message from 'components/Message.jsx'
 import { Box } from 'grommet'
 
 const Chat = () => (
   <Box fill>
     <NavBar title='The Question Box' />
+
+    <Box
+      direction='row'
+      flex
+      justify='center'
+      background='light-3'
+      overflow={{ horizontal: 'hidden' }}
+    >
+      <Box
+        width='large'
+        pad='small'
+      >
+        <Message
+          author='Jonathan Ohayon'
+          message='Test'
+          firstMessage
+        />
+        <Message
+          author='Jonathan Ohayon'
+          message='Test'
+          lastMessage
+        />
+
+        <Message
+          author='Jonathan Ohayon'
+          message='Test'
+          isOwn firstMessage
+        />
+        <Message
+          author='Jonathan Ohayon'
+          message='Test'
+          isOwn lastMessage
+        />
+      </Box>
+    </Box>
   </Box>
 )
 
