0ddb615526e681622198aabc1c40784ba7995698
Jonathan Ohayon <me@jonathano.com>
2019-06-17 09:36:15 +0300

    Started working on the server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { join } = require('path')
+const { json } = require('body-parser')
+const app = express()
+
+app.use(json())
+app.use('/', express.static(join(__dirname, 'dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
