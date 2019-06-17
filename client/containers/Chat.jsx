54b0631c51f187f58dddd28878a57a83a56f4fa7
Jonathan Ohayon <me@jonathano.com>
2019-06-17 15:30:18 +0300

    Added dispatchToProps mapping

--- client/containers/Chat.jsx
+++ client/containers/Chat.jsx
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import NavBar from 'components/NavBar.jsx'
 import Message from 'components/Message.jsx'
+import { getMessages } from 'actions/messages.js'
 import { connect } from 'react-redux'
 import { Box } from 'grommet'
 
 class Chat extends Component {
+  componentDidMount () {
+    this.props.getMessages()
+  }
+
   render () {
     const { messages } = this.props
     console.log(messages)
@@ -54,11 +59,19 @@ class Chat extends Component {
 }
 
 Chat.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.object)
+  messages: PropTypes.arrayOf(PropTypes.object).isRequired,
+  getMessages: PropTypes.func.isRequired
 }
 
 const mapStateToProps = ({ messages }) => ({
   messages: messages.messages
 })
 
-export default connect(mapStateToProps)(Chat)
+const mapDispatchToProps = dispatch => ({
+  getMessages: () => dispatch(getMessages())
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Chat)
