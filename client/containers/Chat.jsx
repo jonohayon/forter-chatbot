09b04ffc9dba332b7e316c4319e3808445636904
Jonathan Ohayon <me@jonathano.com>
2019-06-17 15:14:03 +0300

    Added stateToProps to the Chat container + moved from functional component to a class based one in order to use lifecycle methods

--- client/containers/Chat.jsx
+++ client/containers/Chat.jsx
@@ -1,47 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import NavBar from 'components/NavBar.jsx'
 import Message from 'components/Message.jsx'
+import { connect } from 'react-redux'
 import { Box } from 'grommet'
 
-const Chat = () => (
-  <Box fill>
-    <NavBar title='The Question Box' />
+class Chat extends Component {
+  render () {
+    const { messages } = this.props
+    console.log(messages)
 
-    <Box
-      direction='row'
-      flex
-      justify='center'
-      background='light-3'
-      overflow={{ horizontal: 'hidden' }}
-    >
-      <Box
-        width='large'
-        pad='small'
-      >
-        <Message
-          author='Jonathan Ohayon'
-          message='Test'
-          firstMessage
-        />
-        <Message
-          author='Jonathan Ohayon'
-          message='Test'
-          lastMessage
-        />
+    return (
+      <Box fill>
+        <NavBar title='The Question Box' />
 
-        <Message
-          author='Jonathan Ohayon'
-          message='Test'
-          isOwn firstMessage
-        />
-        <Message
-          author='Jonathan Ohayon'
-          message='Test'
-          isOwn lastMessage
-        />
+        <Box
+          direction='row'
+          flex
+          justify='center'
+          background='light-3'
+          overflow={{ horizontal: 'hidden' }}
+        >
+          <Box
+            width='large'
+            pad='small'
+          >
+            <Message
+              author='Jonathan Ohayon'
+              message='Test'
+              firstMessage
+            />
+            <Message
+              author='Jonathan Ohayon'
+              message='Test'
+              lastMessage
+            />
+
+            <Message
+              author='Jonathan Ohayon'
+              message='Test'
+              isOwn firstMessage
+            />
+            <Message
+              author='Jonathan Ohayon'
+              message='Test'
+              isOwn lastMessage
+            />
+          </Box>
+        </Box>
       </Box>
-    </Box>
-  </Box>
-)
+    )
+  }
+}
 
-export default Chat
+Chat.propTypes = {
+  messages: PropTypes.arrayOf(PropTypes.object)
+}
+
+const mapStateToProps = ({ messages }) => ({
+  messages: messages.messages
+})
+
+export default connect(mapStateToProps)(Chat)
