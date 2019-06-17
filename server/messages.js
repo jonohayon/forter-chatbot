a59b1f54bc36c6ca263f20b35e286a1fe08c51f7
Jonathan Ohayon <me@jonathano.com>
2019-06-17 11:07:16 +0300

    Moved to use lindex from lrange for retrieving the last message

--- server/messages.js
+++ server/messages.js
@@ -4,6 +4,7 @@ const { promisify } = require('util')
 const client = redis.createClient()
 const subClient = client.duplicate()
 const lrange = promisify(client.lrange).bind(client)
+const lindex = promisify(client.lindex).bind(client)
 const rpush = promisify(client.rpush).bind(client)
 const publish = promisify(client.publish).bind(client)
 
@@ -16,7 +17,7 @@ const createMessage = (body, userId) => rpush('messages', JSON.stringify({ body,
 
 const getNMessages = n => lrange('messages', 0, n).then(messages => messages.map(m => JSON.parse(m)))
 
-const getLastMessage = () => lrange('messages', -2, -1).then(messages => JSON.parse(messages[0]))
+const getLastMessage = () => lindex('messages', -1).then(message => JSON.parse(message))
 
 const listenForMessages = handler => {
   subClient.on('message', (channel, message) => {
