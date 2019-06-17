5888d72ec3eb5400e6e1ff0c48e80e4d1cfe2414
Jonathan Ohayon <me@jonathano.com>
2019-06-17 09:40:55 +0300

    Removed explicit route from static file serving

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { json } = require('body-parser')
 const app = express()
 
 app.use(json())
-app.use('/', express.static(join(__dirname, 'dist')))
+app.use(express.static(join(__dirname, '../dist')))
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Listening on port ${port}`))
